9e9d806858f757e5ef964e521a984a6bba888e86
sroslund <scottoslund8@gmail.com>
2020-02-29 13:18:20 -0800

    draw to canvas

--- bird.js
+++ bird.js
@@ -1,10 +1,13 @@
 const cvs = document.getElementById("myCanvas");
 const ctx = cvs.getContext("2d");
 
+const sprite = new Image();
+sprite.src = "flap/monkaS.png";
+
 function draw(){
 	ctx.fillStyle = "#70c5ce";
 	ctx.fillRect(0,0,cvs.width,cvs.height);
-
+	ctx.drawImage(sprite,0,0,75,50,10,10,75,50);
 }
 
 function loop(){
