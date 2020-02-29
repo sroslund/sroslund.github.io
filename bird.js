2dc1ca4ffe8f005b49242fa636a333c23062359a
sroslund <scottoslund8@gmail.com>
2020-02-28 23:18:14 -0800

    added script

--- bird.js
+++ bird.js
@@ -2,12 +2,14 @@ const cvs = document.getElementById("myCanvas");
 const ctx = cvs.getContext("2d");
 
 function draw(){
-
+	ctx.fillStyle = "#70c5ce";
+	ctx.fillRect(0,0,cvs.width,cvs.height);
 
 }
 
 function loop(){
 
-
+	draw();  
   
 }
+loop();
