71df846ad3ee9f950760ca0dee4a77057f4d3556
sroslund <scottoslund8@gmail.com>
2020-02-29 13:54:05 -0800

    changed

--- bird.js
+++ bird.js
@@ -4,10 +4,19 @@ const ctx = cvs.getContext("2d");
 const sprite = new Image();
 sprite.src = "flap/monkaS.png";
 
+const bird = {
+	x: 50,
+	y: 150,
+	draw : function(){
+		ctx.drawImage(sprite,this.x,this.y);
+	}
+
+}
+
 function draw(){
 	ctx.fillStyle = "#70c5ce";
 	ctx.fillRect(0,0,cvs.width,cvs.height);
-	ctx.drawImage(sprite,10,10);
+	bird.draw();
 }
 
 function loop(){
