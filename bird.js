1afbd2aa525a9c265e75dd2b6b9c148d0426c50f
sroslund <scottoslund8@gmail.com>
2020-02-29 20:49:01 -0800

    hi

--- bird.js
+++ bird.js
@@ -19,6 +19,8 @@ function draw(){
 	bird.draw();
 	ctx.font = "30px Arial";
 	ctx.fillText("Hello World", 10,50);
+	ctx.fillStyle = '#70c5ce";
+	ctx.fillRect(0,0,cvs.width,cvs.height);
 }
 
 function loop(){
