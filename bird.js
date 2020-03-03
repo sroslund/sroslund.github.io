ae2c5cf207484d267c82812eb02e9080650919f6
sroslund <scottoslund8@gmail.com>
2020-03-02 22:20:30 -0800

    hi

--- bird.js
+++ bird.js
@@ -1,9 +1,5 @@
-//var canvas = document.getElementById("myCanvas");
-//var ctx = canvas.getContext("2d");
-function startGame() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    img = new Image();
-    img.src = "https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg";
-    ctx.drawImage(img,10,10);
-};
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = 'yellow';
+ctx.fillRect(20,20,100,100);
