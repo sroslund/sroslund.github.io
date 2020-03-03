60c893f226c09153b0310b31dd15420215d60edb
sroslund <scottoslund8@gmail.com>
2020-03-02 22:36:16 -0800

    hi

--- bird.js
+++ bird.js
@@ -1,5 +1,24 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'yellow';
-ctx.fillRect(20,20,100,100);
+const birb = {
+	x: 10,
+	y: 10,
+	dy: -1,
+
+	draw : function(){
+		ctx.fillStyle = 'yellow';
+		ctx.fillRect(this.x,this.y,100,100);
+	},
+	
+	update : funtion(){
+		this.y = this.y - this.dy;
+	}
+}
+
+function loop(){
+	birb.update();
+	birb.draw();
+}
+loop();
+
